f0c72c4e3a86fe1c108117ce62b231de4b3b3993
yehoon17 <yehoon17@gmail.com>
2021-08-31 20:19:58 +0900

    Create project
    Create
     - config.yaml
     - main.py
    
    Add
     - Project instructions
       Toy Project1(DM팀).pdf
     - Data
        2021-03-01.json

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from configparser import ConfigParser
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read("config.yaml")
+    main()
+
