3af25633451a0b18c82dba03cf0dcf81ba7cc118
yehoon17 <yehoon17@gmail.com>
2021-09-01 14:55:58 +0900

    Update normalization.py
    Change normlized_dic datatype to dictionary.

--- src/normalization.py
+++ src/normalization.py
@@ -5,9 +5,9 @@ def normalize(dic, scale=1, digits=0):
     max_val = max(dic.values())
     min_val = min(dic.values())
 
-    normalized_dic = []
+    normalized_dic = {}
     for key, val in dic.items():
         normalized_val = (val - min_val) / (max_val - min_val)
-        normalized_dic.append(round(normalized_val * scale, digits))
+        normalized_dic[key] = round(normalized_val * scale, digits)
 
     return normalized_dic
