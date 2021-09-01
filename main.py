a9dffe81f3c74b1f666ae580e0a07030a3a8d5e7
yehoon17 <yehoon17@gmail.com>
2021-09-01 12:02:29 +0900

    Update main.py
    Complete flow of the programm.

--- main.py
+++ main.py
@@ -2,42 +2,77 @@
 
 import yaml
 import json
-import re
+import numpy as np
+from src.preprocess import preprocess
+from src.tfidf import TfIdfVectorizer
+from src.normalization import normalize
 
+# load configuration
 with open("config.yaml", "r", encoding='utf-8') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
+# set configuration variables
 load_dir = config["load_dir"]
+save_dir = config["save_dir"]
+fields = config["fields"]
 preprocessing = config["preprocessing"]
 word_num = config["word_num"]
 word_order = config["word_order"]
 
-def preprocess_text(text):
-    # Split text to sentences by [!], [?], [.]
-    sentences = re.split("[!?.]",text.strip())
 
-    # Preprocess by option
-    # 1: Remove special char, 2: Remove all except Kor
-    if preprocessing == 1:
-        sentences = [re.sub("[^A-Za-z0-9ㄱ-ㅎ가-힣\s]+","",sentence) for sentence in sentences]
-    elif preprocessing == 2:
-        sentences = [re.sub("[^ㄱ-ㅎ가-힣\s]+","",sentence) for sentence in sentences]
-
-    # Split sentences by space
-    preprocessed_text = []
-    for sentence in sentences:
-        preprocessed_text.append(re.split("\s",sentence.strip()))
-
-    return preprocessed_text
+# get score 
+def get_result(doc_ids, scores):
+    result = {}
+    
+    for doc_id, score in zip(doc_ids, scores):
+        result["DOCID"] = doc_id
+        score_strs = []
+        for key, val in score.items():
+            score_str = "^".join([key,str(val)])
+            score_strs.append(score_str)
+        result["KEYWORD"] = " ".join(score_strs)
+        
+    return result
 
 def main():
-    data = []
-
-    with open(load_dir) as f:
+    # load document data
+    texts = []
+    doc_ids = []
+    with open(load_dir, encoding="utf-8") as f:
         for obj in f:
-            data.append(json.loads(obj))
+            data = json.loads(obj)
+            text = ""
+            for field in fields:
+                text += data[field]
+            texts.append(text)
+            doc_ids.append(data["doc_id"])
 
-    
+    # preprocess data
+    documents = []
+    for text in texts:
+        documents.append(preprocess(text,
+                                    preprocessing,
+                                    split_sentences=False))
+
+    # calculate idf
+    vectorizer = TfIdfVectorizer()
+    vectorizer.fit(documents)
+
+    # get scores 
+    scores = []
+    for document in documents:
+        tfidf = vectorizer.transform(document)
+        normalized_tfidf = normalize(tfidf)
+        scores.append(normalized_tfidf)
+
+    # export result
+    result = get_result(doc_ids, scores)
+
+    with open(save_dir, "w", encoding="utf-8") as json_file:
+        json.dump(result, json_file)
+
+    # analysis
+    pass
 
 
 if __name__ == "__main__":
