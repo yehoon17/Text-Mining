dc6272a413ce6ab0e8cbaa9e5c28183ae698a9f5
yehoon17 <yehoon17@gmail.com>
2021-09-01 18:25:01 +0900

    Update main.py
    Seperated analyzing part.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 
 import yaml
 import json
-import os
 from src.preprocess import preprocess
 from src.tfidf import TfIdfCalculator
 from src.normalization import normalize
+from src.analysis import analysis
 
 # load configuration
 with open("config.yaml", "r", encoding='utf-8') as f:
@@ -99,37 +99,7 @@ def main():
     # analysis
     print("start analysis")
     save_path = "result"
-    # 전체 문서에서 단어 개수 구하여 정렬하여 출력하기 (상위 100개)
-    file_dir = os.path.join(save_path, "tf.txt")
-    with open(file_dir, "w", encoding="utf-8") as f:
-        f.write("\n".join("%s %s" % x 
-                            for x in calculator.get_tf(100)))
-
-    # 각 문서에 가장 많이 등장한 단어 정렬하기 출력하기(상위 100개)
-    file_dir = os.path.join(save_path, "df.txt")
-    with open(file_dir, "w", encoding="utf-8") as f:
-        f.write("\n".join("%s %s" % x 
-                            for x in calculator.df.most_common(100)))
-
-    # 단어 스코어 상위 10개 기준으로 스코어 편차가 가장 많이 나는 문서 구하기
-    file_dir = os.path.join(save_path, "diff.txt")
-    with open(file_dir, "w", encoding="utf-8") as f:
-        pass
-
-    # 스코어 100점이 가능 많은 문서대로 나열하기
-    file_dir = os.path.join(save_path, "doc.txt")
-    with open(file_dir, "w", encoding="utf-8") as f:
-        pass
-
-    # 전체문서에서 단어 길이가 긴 순서대로 정렬하기(상위10개)
-    file_dir = os.path.join(save_path, "long.txt")
-    with open(file_dir, "w", encoding="utf-8") as f:
-        pass
-
-    # 전체문서에서 한글자 단어 출력하기
-    file_dir = os.path.join(save_path, "one.txt")
-    with open(file_dir, "w", encoding="utf-8") as f:
-        pass
+    analysis(calculator, save_path)
 
 
 if __name__ == "__main__":
