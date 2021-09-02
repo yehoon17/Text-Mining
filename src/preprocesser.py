28e7c39f1d598c4a84bc330ee260c95aa4f22e12
yehoon17 <yehoon17@gmail.com>
2021-09-03 02:03:39 +0900

    Rename preprocess.py as preprocesser.py
    Made it as class.

---
+++ src/preprocesser.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+
+class NotValidPreprocessError(Exception):
+    def __init__(self):
+        super().__init__("NotValidPreprocessError")
+
+
+class Preprocesser:
+    def __init__(self, preprocessing):
+        self.preprocessing = preprocessing
+
+    # preprocess text
+    def preprocess(self, text, split_sentences=True):
+        if split_sentences:
+            # Split text to sentences by [!], [?], [.]
+            sentences = re.split("[!?.]", text.strip())
+
+            # Preprocess by option
+            # 1: Remove special char, 2: Remove all except Kor
+            if self.preprocessing == 1:
+                sentences = [re.sub("[^A-Za-z0-9ㄱ-ㅎ가-힣\s]+", " ", sentence) 
+                for sentence in sentences]
+            elif self.preprocessing == 2:
+                sentences = [re.sub("[^ㄱ-ㅎ가-힣\s]+", " ", sentence)
+                for sentence in sentences]
+            else:
+                raise NotValidPreprocessError
+
+            # Split sentences by space
+            preprocessed_text = []
+            for sentence in sentences:
+                preprocessed_text.append(re.split("\s", sentence.strip()))
+
+        else:
+            # Preprocess by option
+            # 1: Remove special char, 2: Remove all except Kor
+            if self.preprocessing == 1:
+                text = re.sub("[^A-Za-z0-9ㄱ-ㅎ가-힣\s]+", " ", text)
+            elif self.preprocessing == 2:
+                text = re.sub("[^ㄱ-ㅎ가-힣\s]+", " ", text)
+            else:
+                raise NotValidPreprocessError
+
+            # Split sentences by space
+            preprocessed_text = text.strip().split()
+
+        return preprocessed_text
