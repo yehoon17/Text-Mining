c1e621bd571336b507c35c5dfee1823e5d864053
yehoon17 <yehoon17@gmail.com>
2021-09-03 02:02:26 +0900

    Rename normaliztion.py to normalizer.py
    Made it as class.

---
+++ src/normalizer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Normalizer:
+    def __init__(self, scale=1):
+        self.scale = scale
+        
+    def normalize(self, dic, digits=0):
+        max_val = max(dic.values())
+        min_val = min(dic.values())
+
+        normalized_dic = {}
+        for key, val in dic.items():
+            normalized_val = (val - min_val) / (max_val - min_val)
+            normalized_dic[key] = round(normalized_val * self.scale, digits)
+
+        return normalized_dic
